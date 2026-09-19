427d316a851dd984463e38af04aa2a90b309198b
kirudevv <ruimochii@gmail.com>
2026-09-20 05:52:26 +0800

    project setup

---
+++ junaieflowershop-sad/src/main/java/com/gplsadgroup/App.java
@@ -0,0 +1,13 @@
+package com.gplsadgroup;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
